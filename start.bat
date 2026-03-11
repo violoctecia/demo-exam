b0d33321291ba00a1bd3242a99593867169c0835
tonight80 <m1sty32@yandex.ru>
2026-03-11 13:06:56 +0300

    fixes

--- start.bat
+++ start.bat
@@ -1,7 +1,14 @@
 @echo off
 echo === Загрузка образов из файлов ===
-docker load -i app-dev.tar
-docker load -i postgres.tar
+docker load -i flash\app-dev.tar
+docker load -i flash\postgres.tar
+
+echo === Переименование образа ===
+docker tag demo-exam-app-dev:latest korochki-app-dev:latest 2>nul
+docker tag demo_easy-app-dev:latest korochki-app-dev:latest 2>nul
+
+echo === Подготовка конфигурации ===
+if not exist .env copy .env.example .env
 
 echo === Запуск приложения ===
 docker-compose --profile dev up -d
